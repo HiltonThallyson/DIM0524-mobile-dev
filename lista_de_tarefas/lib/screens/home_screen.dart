9d10e830525b1e8f6f8223b45c98c80bfd01b78f
HiltonThallyson <hilton.thallyson@gmail.com>
2022-10-26 14:17:19 -0300

    feat-add,remove, persistance added

--- lista_de_tarefas/lib/screens/home_screen.dart
+++ lista_de_tarefas/lib/screens/home_screen.dart
@@ -27,6 +27,26 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void _addToDo() async {
+    setState(() {
+      Map<String, dynamic> newTodo = {};
+      newTodo["title"] = _todoController.text;
+      newTodo["ok"] = false;
+      _todoList.add(newTodo);
+      _todoController.text = "";
+      DataHandler.saveData(_todoList);
+    });
+  }
+
+  void _removeToDo(int index) async {
+    _lastRemoved = Map.from(_todoList[index]);
+    _lastRemovedPos = index;
+    setState(() {
+      _todoList.removeAt(index);
+      DataHandler.saveData(_todoList);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,21 +60,62 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                const TextField(
-                  decoration: InputDecoration(
-                      labelText: "Nome da tarefa",
-                      labelStyle: TextStyle(color: Colors.blueAccent)),
+                Expanded(
+                  child: TextField(
+                    controller: _todoController,
+                    decoration: const InputDecoration(
+                        labelText: "Nome da tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
+                  ),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: _addToDo,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.blueAccent,
                   ),
                   child: const Text("Add"),
                 )
               ],
-            )
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _todoList.length,
+                itemBuilder: (context, index) => Dismissible(
+                  key: Key(DateTime.now().microsecondsSinceEpoch.toString()),
+                  background: Container(
+                    padding: const EdgeInsets.only(left: 10),
+                    alignment: Alignment.centerLeft,
+                    color: Colors.red,
+                    child: const Icon(
+                      Icons.delete,
+                      size: 32,
+                      color: Colors.white,
+                    ),
+                  ),
+                  direction: DismissDirection.startToEnd,
+                  onDismissed: (_) => _removeToDo(index),
+                  child: CheckboxListTile(
+                    title: Text(
+                      _todoList[index]["title"],
+                      style: const TextStyle(color: Colors.blueAccent),
+                    ),
+                    secondary: CircleAvatar(
+                        backgroundColor: Colors.blueAccent,
+                        child: Icon(_todoList[index]["ok"]
+                            ? Icons.check
+                            : Icons.error)),
+                    value: _todoList[index]["ok"],
+                    onChanged: ((value) {
+                      setState(() {
+                        _todoList[index]["ok"] = !_todoList[index]["ok"];
+                      });
+                    }),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
